2a7106286b9ac8cbeccb8c120bf3878b9c08fa09
Luiyit Hernandez <luiyit.hernandez@widget-pc.com>
2019-11-16 12:22:51 -0500

    Update default css packages and clenup gulp tasks

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ Simple skeleton to start a custom wordpress theme using Timber.
 3. Rename the folder to something that makes sense for your website (generally no spaces and all lowercase). You could keep the name wp-timber-boilerplate but the point of a starter theme is to make it your own!
 4. Activate the theme in Appearance > Themes.
 5. Install packages (npm i) and run npm start for start development.
+
+
+# CSS packages included
+1. [Bootstrap](https://github.com/twbs/bootstrap). Only grid system. You can include more modules updating _vendor.scss file
+2. [Font Awesome](https://github.com/FortAwesome/Font-Awesome).
--- gulpfile.js
+++ gulpfile.js
@@ -14,39 +14,29 @@ const webpackStream = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
 
 var input = './src/assets/scss/*.scss';
-var output = './public/assets/css';
+var output = './public/css';
 
 var sassOptions = {
   errLogToConsole: true,
-  outputStyle: 'expanded'
+  outputStyle: 'compressed'
 };
 
 var autoprefixerOptions = {
   browsers: ['last 3 versions', 'IE 9', 'IE 10', 'IE 11']
 };
 
-gulp.task('serve', ['sass', 'sass:min', 'image', 'nunjucks', 'js', 'js:watch'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./public"
-        }
-    });
-
-    gulp.watch("./src/assets/scss/**/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch(["./src/assets/js/**/*.js", "./src/assets/js/*.js"]).on('change', browserSync.reload);
+gulp.task('serve', ['icons', 'sass', 'sass:min', 'image', 'js', 'js:watch'], function() {
+    gulp.watch("./src/assets/scss/**/*.scss", ['sass'])
+    gulp.watch(["./src/assets/js/**/*.js", "./src/assets/js/*.js"])
     gulp.watch("./src/assets/img/**", function() {
         gulp.run('image');
-        browserSync.realod;
     })
-    // gulp.watch("./public/*.html").on('change', browserSync.reload);
-	gulp.watch('./src/app/**/*.html', ['nunjucks']).on('change', browserSync.reload);
 });
-
 
 gulp.task('js', () => {
     gulp.src('./src/assets/js/main.js')
         .pipe(webpackStream(webpackConfig), webpack)
-        .pipe(gulp.dest('./public/assets/js'));
+        .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('js:watch', function () {
@@ -56,46 +46,40 @@ gulp.task('js:watch', function () {
 gulp.task('image:prod', ['clean:image'], function () {
     return gulp.src('./src/assets/img/**')
         .pipe(image())
-        .pipe(gulp.dest('./public/assets/img'));
+        .pipe(gulp.dest('./public/img'));
 });
 
 gulp.task('image', ['clean:image'], function () {
     return gulp.src('./src/assets/img/**')
-        .pipe(gulp.dest('./public/assets/img'));
+        .pipe(gulp.dest('./public/img'));
 });
 
 gulp.task('clean:image', function() {
-    return del(['./public/assets/img/**', '!public/assets/img'], {force:true})
+    return del(['./public/img/**', '!public/img'], {force:true})
 });
 
 gulp.task('sass', function() {
     return gulp
         .src(input)
-        // .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(sass(sassOptions).on('error', sass.logError))
-        // .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write())
         .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest(output))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest(output));
 });
 
 gulp.task('sass:min', function() {
     // return gulp
     //     .src(input)
-    //     .pipe(sass({ outputStyle: 'nested' }))
+    //     .pipe(sass({ outputStyle: 'compressed' }))
     //     .pipe(autoprefixer(autoprefixerOptions))
     //     .pipe(csso())
     //     .pipe(gulp.dest(`${output}`));
 });
-gulp.task('nunjucks', () => {
-    return gulp.src('./src/app/**/*.html')
-        .pipe(prettify({
-            indent_size : 4 // размер отступа - 4 пробела
-        }))
-        .pipe(njkRender({
-            path: ['./src/app/']
-        }))
-        .pipe(gulp.dest('./public'));
+
+gulp.task('icons', function() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+               .pipe(gulp.dest('./public/webfonts/'));
 });
 
 gulp.task('watch', function() {
@@ -107,5 +91,4 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['serve']);
-
 gulp.task('prod', ['image:prod']);

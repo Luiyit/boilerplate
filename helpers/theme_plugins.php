a3ee892b7bf01064915d123f8766ad07770b166d
luiyit <luiyit.hernandez@widget-pc.com>
2020-02-20 19:53:49 -0500

    Setup basic recommended and required  plugins

--- helpers/theme_plugins.php
+++ helpers/theme_plugins.php
@@ -3,11 +3,41 @@
 function add_theme_plugins(){
   $plugins = array(
 
-		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'     => 'Timber',
+			'slug'     => 'timber-library',
+			'required' => true,
+		),
 		array(
 			'name'     => 'iThemes Security',
 			'slug'     => 'better-wp-security',
-			'required' => true,
+			'required' => false,
+		),
+		array(
+			'name'     => 'Yoast SEO',
+			'slug'     => 'wordpress-seo',
+			'required' => false,
+		),
+		array(
+			'name'     => 'Disable Gutenberg',
+			'slug'     => 'disable-gutenberg',
+			'required' => false,
+		),
+		array(
+			'name'     => 'MC4WP: Mailchimp for WordPress',
+			'slug'     => 'mailchimp-for-wp',
+			'required' => false,
+		),
+		array(
+			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
+			'slug'               => 'advanced-custom-fields-pro-master', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/advanced-custom-fields-pro-master.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '5.8.7', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
   );
 
@@ -30,84 +60,7 @@ function add_theme_plugins(){
 		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
 		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
-
-		/*
-		'strings'      => array(
-			'page_title'                      => __( 'Install Required Plugins', 'theme-slug' ),
-			'menu_title'                      => __( 'Install Plugins', 'theme-slug' ),
-			// translators: %s: plugin name.
-			'installing'                      => __( 'Installing Plugin: %s', 'theme-slug' ),
-			// translators: %s: plugin name.
-			'updating'                        => __( 'Updating Plugin: %s', 'theme-slug' ),
-			'oops'                            => __( 'Something went wrong with the plugin API.', 'theme-slug' ),
-			'notice_can_install_required'     => _n_noop(
-				// translators: 1: plugin name(s).
-				'This theme requires the following plugin: %1$s.',
-				'This theme requires the following plugins: %1$s.',
-				'theme-slug'
-			),
-			'notice_can_install_recommended'  => _n_noop(
-				// translators: 1: plugin name(s).
-				'This theme recommends the following plugin: %1$s.',
-				'This theme recommends the following plugins: %1$s.',
-				'theme-slug'
-			),
-			'notice_ask_to_update'            => _n_noop(
-				// translators: 1: plugin name(s).
-				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-				'theme-slug'
-			),
-			'notice_ask_to_update_maybe'      => _n_noop(
-				// translators: 1: plugin name(s).
-				'There is an update available for: %1$s.',
-				'There are updates available for the following plugins: %1$s.',
-				'theme-slug'
-			),
-			'notice_can_activate_required'    => _n_noop(
-				// translators: 1: plugin name(s).
-				'The following required plugin is currently inactive: %1$s.',
-				'The following required plugins are currently inactive: %1$s.',
-				'theme-slug'
-			),
-			'notice_can_activate_recommended' => _n_noop(
-				// translators: 1: plugin name(s).
-				'The following recommended plugin is currently inactive: %1$s.',
-				'The following recommended plugins are currently inactive: %1$s.',
-				'theme-slug'
-			),
-			'install_link'                    => _n_noop(
-				'Begin installing plugin',
-				'Begin installing plugins',
-				'theme-slug'
-			),
-			'update_link' 					  => _n_noop(
-				'Begin updating plugin',
-				'Begin updating plugins',
-				'theme-slug'
-			),
-			'activate_link'                   => _n_noop(
-				'Begin activating plugin',
-				'Begin activating plugins',
-				'theme-slug'
-			),
-			'return'                          => __( 'Return to Required Plugins Installer', 'theme-slug' ),
-			'plugin_activated'                => __( 'Plugin activated successfully.', 'theme-slug' ),
-			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'theme-slug' ),
-			// translators: 1: plugin name.
-			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'theme-slug' ),
-			// translators: 1: plugin name.
-			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'theme-slug' ),
-			// translators: 1: dashboard link.
-			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'theme-slug' ),
-			'dismiss'                         => __( 'Dismiss this notice', 'theme-slug' ),
-			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'theme-slug' ),
-			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'theme-slug' ),
-
-			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
-		),
-		*/
+		'message'      => '',                      // Message to output right before the plugins table.s
 	);
 
 	tgmpa( $plugins, $config );

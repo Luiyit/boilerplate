b0eecdd70707dbb0a039d5ff374dd598303b614c
Luiyit <luiyit.hernandez@widget-pc.com>
2020-10-29 07:13:57 -0500

    Add support to page templates

--- helpers/filters.php
+++ helpers/filters.php
@@ -1,7 +1,15 @@
 <?php
-/** Include SVG support */
+
+/* Include SVG support */
 function add_svg_mime_types($mimes) {
   $mimes['svg'] = 'image/svg+xml';
   return $mimes;
 }
 add_filter('upload_mimes', 'add_svg_mime_types');
+
+/* Add custom template */
+function add_page_template ($templates) {
+  $templates['sections-template.twig'] = 'Sectiosn page';
+  return $templates;
+}
+add_filter ('theme_page_templates', 'add_page_template');
